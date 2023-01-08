f7bc9966ba248b684011c136c48dd8f1748d3b58
Aleksandr Zotov <szotov223@gmail.com>
2023-01-09 03:12:33 +0500

    ver 1.0.1

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/1_tnQAIGWPqJfA9DWBnzalpQZ81Q34U3X/view?usp=share_link"
                 >
                   Резюме
                 </a>
